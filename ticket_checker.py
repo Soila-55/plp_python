0ad9e258e268758b0413f938605508152d0e747a
Soila-55 <gloriatago4@gmail.com>
2026-07-17 16:58:59 +0300

    Add age check for ticket pricing

--- ticket_checker.py
+++ ticket_checker.py
@@ -1,1 +1,14 @@
+# Ask the user for their age
+age = int(input("Enter your age: "))
 
+# Store a Boolean value
+is_adult = age >= 18
+
+# Display the Boolean
+print("Is Adult:", is_adult)
+
+# Decide ticket price
+if is_adult:
+    print("Ticket Price: Ksh 500")
+else:
+    print("Ticket Price: Ksh 250")
